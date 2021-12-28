f3f1a82d1baef434550002320d3a6b7109d8a49e
rui-watanabe <wataru610806@gmail.com>
2021-12-28 14:48:38 +0900

    add authController #3

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,7 +1,33 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"react-go-auth2/database"
+	"react-go-auth2/models"
 
-func Hello(c *fiber.Ctx) error {
-	return c.SendString("Hello, World !")
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// func Hello(c *fiber.Ctx) error {
+// 	return c.SendString("Hello, World !")
+// }
+
+func Register(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+
+	user := models.User{
+		Name:     data["name"],
+		Email:    data["email"],
+		Password: password,
+	}
+
+	database.DB.Create(&user)
+
+	return c.JSON(user)
 }
