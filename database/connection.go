dda4a6d25335a5e0c301c72a9354268f1b307238
rui-watanabe <wataru610806@gmail.com>
2021-12-28 14:49:03 +0900

    add db connection #3

--- database/connection.go
+++ database/connection.go
@@ -2,17 +2,24 @@ package database
 
 import (
 	"react-go-auth2/config"
+	"react-go-auth2/models"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func Connect() {
 	dsn := config.Config.DBUser + ":" + config.Config.DBPass + "@tcp(" + config.Config.DBHost + ")/" + config.Config.DBName + "?charset=utf8&parseTime=True&loc=Local"
 	// dsn = "root@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err.Error())
 	}
+
+	DB = connection
+
+	connection.AutoMigrate(&models.User{})
 }
